21e968f14fdd889e9ee94a06893435f8fe791c64
dizelrr <dizel-rr@mail.ru>
2024-02-09 18:28:31 +0300

    Урок 1.2

--- Lesson1/Task2/Massiv.java
+++ Lesson1/Task2/Massiv.java
@@ -6,21 +6,36 @@ package Lesson1.Task2;
 public class Massiv {
       public static void main(String[] args) {
             int [] array = {1,1,0,1,1,1};
-            int count = 0;
-            int maxCount = 0;
-            for (int i = 0; i < array.length; i++) {
-                  if (array[i] ==1){
-                        count ++;
-                  
-                        if (count > maxCount){
-                        maxCount = count;
-                        }
-                  }
-                  else {
-                        count = 0;
-                  }
-                  
-            } 
+            int maxCount = getMaxOnes(array);
             System.out.println(maxCount);
       }
-}
+            private static int getMaxOnes(int[] array) {
+                  int count = 0;
+                  int maxCount = 0;
+                  for (int j : array) {
+                        if (j == 1) count++;
+                        else {
+                        count = 0;
+                        }
+                        if (maxCount < count) {
+                        maxCount = count;
+                        }
+                        }
+                  return maxCount;
+            } 
+                  
+            }
+      
+
+            // for (int i = 0; i < array.length; i++) {
+            //       if (array[i] ==1){
+            //             count ++;
+                  
+            //             // // if (count > maxCount){
+            //             // // maxCount = count;
+            //             // }
+            //             maxCount = count > maxCount ? count : maxCount; // второй варинт  . сверху первый вариант закомичен
+            //       }
+            //       else {
+            //             count = 0;
+            //       }
